073ea2cd6c8cb950b57b5052763185243174954c
Jon Corn <jdcorn@live.com>
2022-05-15 11:36:26 -0700

    fetch user network call

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -56,4 +56,43 @@ class NetworkManager {
         }
         task.resume()
     }
+    
+    func getUserInfo(for username: String, completed: @escaping (Result<User, GFError>) -> Void) {
+        
+        // BUILD URL
+        let endpoint = baseURL + "\(username)"
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.invalidUsername))
+            return
+        }
+
+        // URLSESSION DATATASK
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            // Handle error
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            // Handle response
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            // Handle data
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            // decode data, catch possible error
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
 }
