867c51144468fe5e1c4dd6adcaa6942d0816d4ad
Jon Corn <jdcorn@live.com>
2022-05-16 12:03:09 -0700

    set up UserInfoHeaderVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Jon Corn on 5/16/22.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
